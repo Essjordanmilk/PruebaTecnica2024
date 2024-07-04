29f01188b1f6aafac9f08eddcbc952f6eb5cf98d
Essjordanmilk <jordanmc285@gmail.com>
2024-07-04 17:25:02 -0500

    [Change]Se agrega routing entre vistas para mayor comodidad de visualizacion de la prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { FormUserComponent } from './components/form-user/form-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-list', component: UserListComponent },
+  { path: 'form-user', component: FormUserComponent },
+  { path: '', component: UserListComponent }, // No redirigir a ninguna ruta por defecto
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Ruta comodín para rutas no definidas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
